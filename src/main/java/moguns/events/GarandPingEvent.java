835a050e5db35d283ed0c91bd89b912575d0548a
Bomb787 <39660651+Bomb787@users.noreply.github.com>
2022-12-30 16:51:45 -0500

    Update GarandPingEvent.java

--- src/main/java/moguns/events/GarandPingEvent.java
+++ src/main/java/moguns/events/GarandPingEvent.java
@@ -16,20 +16,21 @@ import net.minecraftforge.fml.common.Mod;
  */
 @Mod.EventBusSubscriber(modid = MoGuns.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class GarandPingEvent {
-
+	
     @SubscribeEvent
     public static void postShoot(Post event) {
-
+    	
         Player player = event.getEntity();
         ItemStack heldItem = player.getMainHandItem();
         CompoundTag tag = heldItem.getTag();
-
+        
         if (heldItem.getItem() == ItemInit.M1_GARAND.get() && tag != null) {
-
+        	
             if (tag.getInt("AmmoCount") == 1)
                 event.getEntity().level.playSound(player, player.blockPosition(), SoundInit.GARAND_PING.get(), SoundSource.MASTER, 3.0F, 1.0F);
-
+            
         }
+        
     }
-
+    
 }
