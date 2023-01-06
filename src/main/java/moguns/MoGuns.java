59434dc4672007ba99d12752bb359ac9df21345c
Bomb787 <39660651+Bomb787@users.noreply.github.com>
2023-01-06 18:22:02 -0500

    Fixed crashing on servers

--- src/main/java/moguns/MoGuns.java
+++ src/main/java/moguns/MoGuns.java
@@ -41,12 +41,9 @@ import moguns.init.EntityInit;
 import moguns.init.ItemInit;
 import moguns.init.ParticleInit;
 import moguns.init.SoundInit;
-import moguns.particles.FireballParticle;
-import moguns.particles.FlareSmokeParticle;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -93,7 +90,6 @@ public class MoGuns {
 		ModLootModifiers.register(bus);
 		
 		bus.addListener(this::onClientSetup);
-		bus.addListener(this::onParticlesRegistry);
 		
 	}
 	
@@ -108,11 +104,6 @@ public class MoGuns {
 		ProjectileManager.getInstance().registerFactory(ItemInit.FLARE.get(), ((world, livingEntity, itemStack, gunItem, gun) -> new FlareProjectileEntity(EntityInit.FLARE.get(), world, livingEntity, itemStack, gunItem, gun)));
 			
 	}
-	
-	private void onParticlesRegistry(RegisterParticleProvidersEvent event) {
-		event.register(ParticleInit.FIREBALL_PARTICLES.get(), FireballParticle.Provider::new);
-		event.register(ParticleInit.FLARE_SMOKE.get(), FlareSmokeParticle.Provider::new);
-    }
 	
 	//This is the client setup event.
 	private void onClientSetup(FMLClientSetupEvent event) {
--- src/main/java/moguns/init/ParticleInit.java
+++ src/main/java/moguns/init/ParticleInit.java
@@ -1,12 +1,20 @@
 package moguns.init;
 
 import moguns.MoGuns;
+import moguns.particles.FireballParticle;
+import moguns.particles.FlareSmokeParticle;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+@Mod.EventBusSubscriber(modid = MoGuns.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ParticleInit {
     public static final DeferredRegister<ParticleType<?>> PARTICLES =
             DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, MoGuns.MOD_ID);
@@ -15,5 +23,12 @@ public class ParticleInit {
             PARTICLES.register("fireball", () -> new SimpleParticleType(true));
     public static final RegistryObject<SimpleParticleType> FLARE_SMOKE =
             PARTICLES.register("flare_smoke", () -> new SimpleParticleType(true));
+    
+    @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
+    public static void onParticlesRegistry(RegisterParticleProvidersEvent event) {
+		event.register(ParticleInit.FIREBALL_PARTICLES.get(), FireballParticle.Provider::new);
+		event.register(ParticleInit.FLARE_SMOKE.get(), FlareSmokeParticle.Provider::new);
+    }
     
 }
